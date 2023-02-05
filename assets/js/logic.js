6ff886ef175cf0f40eeb60d18f4b364d3cde78a2
Your Name <you@example.com>
2023-02-05 22:25:09 +0000

    Timer added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,14 +1,25 @@
+// Selects element by class
+var timeEl = document.getElementById("time");
+
+// Selects element by id
+var startBtn = document.getElementById("start");
+
+startBtn.addEventListener("click", function(){
+  setTime();
+});
+
+var secondsLeft = 60;
+
 function setTime() {
     // Sets interval in variable
     var timerInterval = setInterval(function() {
       secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
+      timeEl.textContent = secondsLeft;
   
       if(secondsLeft === 0) {
         // Stops execution of action at set interval
         clearInterval(timerInterval);
         // Calls function to create and append image
-        sendMessage();
       }
   
     }, 1000);
