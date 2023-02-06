d90aa32416e96319671dadb1cab868a5f6b798fc
Your Name <you@example.com>
2023-02-06 20:43:48 +0000

    Question functionality done

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,5 @@
+var questionNo = 0;
+var timerInterval;
 // Selects element by class
 var timeEl = document.getElementById("time");
 // Selects element by id
@@ -13,20 +15,70 @@ startBtn.addEventListener("click", function () {
     div.className = "start";
     div2.className = "hide";
   }
+  questionCall();
 });
 
 var secondsLeft = 60;
 
 function setTime() {
   // Sets interval in variable
-  var timerInterval = setInterval(function () {
+  timerInterval = setInterval(function () {
     secondsLeft--;
     timeEl.textContent = secondsLeft;
 
-    if (secondsLeft === 0) {
+    if (secondsLeft <= 0) {
       // Stops execution of action at set interval
+      let div = document.getElementById("end-screen");
+      let div2 = document.getElementById("questions");
+      div.className = "start";
+      div2.className = "hide";
       clearInterval(timerInterval);
-      // Calls function to create and append image
     }
   }, 1000);
 }
+
+
+function check_ans(event){
+  if(questionNo >= questions.length-1){
+    let div = document.getElementById("end-screen");
+    let div2 = document.getElementById("questions");
+    div.className = "start";
+    div2.className = "hide";
+    document.getElementById('final-score').textContent = secondsLeft;
+    // clearTimeout(timerInterval);
+    
+  }
+  else{
+    if(event.target.innerText  === questions[questionNo].correctAnswer){
+      questionNo++;
+      questionCall()
+    }
+    else{
+      secondsLeft=secondsLeft-5;
+      questionNo++;
+      questionCall()
+    }
+
+  }
+ 
+
+}
+
+function questionCall() {
+  var question_title = document.getElementById("question-title");
+  question_title.textContent = questions[questionNo].question;
+  document.getElementById('choices').textContent = '';
+  for(var i=0;i<4;i++){
+  var options = document.createElement('button');
+  options.addEventListener('click', check_ans);
+  options.textContent= questions[questionNo].options[i];
+  document.getElementById('choices').append(options);
+  }
+
+}
+
+
+
+
+
+
---
+++ assets/js/question.js
@@ -0,0 +1,26 @@
+let questions = [{
+    question: "What is the capital of France?",
+    options: [
+      "Paris",
+      "London",
+      "Rome",
+      "Berlin"
+    ],
+    correctAnswer: "Paris"
+  },
+  {
+  question: "What is the capital of Bangladesh?",
+  options: [
+    "Paris",
+    "London",
+    "Dhaka",
+    "Berlin"
+  ],
+  correctAnswer: "Dhaka"
+}
+];
+
+
+
+
+  
